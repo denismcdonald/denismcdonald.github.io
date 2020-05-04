a14413b6d904a7e48a3edb22661d3bab96538341
Denis McDonald <denis.l.mcdonald@gmail.com>
2020-05-04 19:49:50 +1000

    Add onfocus

--- _site/js/index-script.js
+++ _site/js/index-script.js
@@ -1,32 +1,43 @@
 console.log("c64 design by Reg Tait: regmtait.co.uk");
 
-function printLetterByLetter(destination, message, speed){
-    var i = 0;
-    var interval = setInterval(function(){
-        document.getElementById(destination).innerHTML += message.charAt(i);
-        i++;
-        if (i > message.length){
-            clearInterval(interval);
+var loaded = false;
+
+window.onfocus = function () { 
+
+    if(!loaded) {
+
+        function printLetterByLetter(destination, message, speed){
+            var i = 0;
+            var interval = setInterval(function(){
+                document.getElementById(destination).innerHTML += message.charAt(i);
+                i++;
+                if (i > message.length){
+                    clearInterval(interval);
+                }
+            }, speed);
         }
-    }, speed);
+
+        setTimeout(function() {
+            printLetterByLetter("load", "Load\"$\",8", 50);
+        }, 100);
+
+        setTimeout(function() {
+            document.querySelector("#page-heading").style.visibility = "visible";
+        }, 700)
+
+        setTimeout(function() {
+            printLetterByLetter("list", "List", 140);
+        }, 700);
+
+        setTimeout(function() {
+            setInterval(function() {
+                document.querySelector("#post-list").style.visibility = "visible";
+                document.getElementById("cursor").classList.toggle("blink");
+            }, 600);
+        }, 800)
+
+    loaded = true;
+    
+    }
+
 }
-
-setTimeout(function() {
-    printLetterByLetter("load", "Load\"$\",8", 50);
-}, 100);
-
-setTimeout(function() {
-    document.querySelector("#page-heading").style.visibility = "visible";
-}, 700)
-
-setTimeout(function() {
-    printLetterByLetter("list", "List", 140);
-}, 700);
-
-setTimeout(function() {
-    setInterval(function() {
-        document.querySelector("#post-list").style.visibility = "visible";
-        document.getElementById("cursor").classList.toggle("blink");
-    }, 600);
-}, 800)
-
