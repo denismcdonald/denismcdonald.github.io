1fddf9517ed1d50d3fc5c6eba55ca7128eac8f67
Denis McDonald <denis.l.mcdonald@gmail.com>
2020-05-16 20:40:15 +1000

    Add SSM

--- js/archive-script.js
+++ js/archive-script.js
@@ -39,3 +39,59 @@ document.addEventListener("visibilitychange", function() {
         loadList();
     }
 });
+
+var c = document.getElementById("cursor");
+var text = document.getElementById("text");
+var secret = document.getElementById("secret");
+var close = document.getElementById("close");
+var frame = document.getElementById("frame");
+var expanded = false;
+
+c.addEventListener("mousedown", function() {
+    if (expanded == false) {
+        text.style.display = "inline-block";
+        c.style.visibility = "hidden";
+        printLetterByLetter("text", "\xa0SUPER SECRET MENU\xa0", 25);
+        setTimeout(function() {
+            document.getElementById("secret").style.visibility = "visible";
+            expanded = true;
+        }, 500)
+    } else {
+        return;
+    }
+});
+
+text.addEventListener("mousedown", function() {
+    if (expanded == true) {
+        text.innerText = "";
+        c.style.visibility = "visible";
+        expanded = false;
+        secret.style.visibility = "hidden";
+        frame.style.display = "none";
+    }
+});
+
+close.addEventListener("mousedown", function() {
+    if (expanded == true) {
+        text.innerText = "";
+        c.style.visibility = "visible";
+        expanded = false;
+        secret.style.visibility = "hidden";
+        frame.style.display = "none";
+    }
+});
+
+function commando() {
+    frame.style.display = "block";
+    frame.src = "https://www.youtube.com/embed/qrQuR1LHAVI";
+}
+
+function thing() {
+    frame.style.display = "block";
+    frame.src = "https://www.youtube.com/embed/mmmEBO2lDVI";
+}
+
+function ninja() {
+    frame.style.display = "block";
+    frame.src = "https://www.youtube.com/embed/1ok1ChTtWQI";
+}
